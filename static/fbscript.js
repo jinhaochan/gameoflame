eb16b4474bc0cd8970a8819cf7870168985cfb89
User <jinhao_@hotmail.com>
2018-01-16 20:05:59 +0800

    add fb

--- static/fbscript.js
+++ static/fbscript.js
@@ -64,6 +64,9 @@
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
+    $.post('/update_session/', function(data) {
+    alert(data);
+    });
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
       });
