f0705a89fec7c4b4be3f6058837086a88558cd45
User <jinhao_@hotmail.com>
2018-01-28 13:44:13 +0800

    added in db

--- vjj/settings.py
+++ vjj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.dreamrlog.com','.herokuapp.com']
 
+DATABASES = { 'default': dj_database_url.config() }
 
 # Application definition
 
@@ -75,14 +77,6 @@ WSGI_APPLICATION = 'vjj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
