da98d9cfe7236851e321eeb194056089b02fdf1a
User <jinhao_@hotmail.com>
2018-01-28 09:25:42 +0800

    updated site

--- vjj/forms.py
+++ vjj/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class suggestion(forms.Form):
-    suggestion = forms.CharField(label='suggestion', max_length=200)
+    suggestion = forms.CharField(label='What weird stuff do you have in mind?', max_length=200)
