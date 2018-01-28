46e1281edd100749edfe4c1e95c832c033bff25e
User <jinhao_@hotmail.com>
2018-01-28 14:32:02 +0800

    working on models

--- vjj/models.py
+++ vjj/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 class Suggestion(models.Model):
+    class Meta:
+        app_label = "vjj"
     name = models.CharField(max_length=30)
     suggestion = models.CharField(max_length=200)
